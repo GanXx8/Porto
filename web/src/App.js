70d3545119f324d600b7cbcf710005fd25cbd184
GanXx8 <ganda.imasxx@gmail.com>
2021-11-12 00:24:25 +0700

    config react js

---
+++ web/src/App.js
@@ -0,0 +1,11 @@
+import Webintro from "./pages/webintro";
+
+function App() {
+  return (
+    <div className="App">
+      <Webintro/>
+    </div>
+  );
+}
+
+export default App;
---
+++ web/src/pages/webintro.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Webintro(){
+    return(
+        <div className="colors.indigo">
+            <div className="headerintro">
+                <h1>hallo world</h1>
+            </div>
+            <div className="main">
+                <h1>ini maincontent</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Webintro;
